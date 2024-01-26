2fc2407230251b4392e89b283c997983f05f7d9c
Syed <72160793+ssa17@users.noreply.github.com>
2024-01-26 11:29:07 +0000

    we can process one line of text
    - [ ] character sequence
    - [ ] exception flow control
    - [ ] list literal
    - [ ] multiline

--- src/main/java/aoc/day01/Day01.java
+++ src/main/java/aoc/day01/Day01.java
@@ -2,6 +2,7 @@ package aoc.day01;
 
 import aoc.Day;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Day01 extends Day {
@@ -16,7 +17,19 @@ public class Day01 extends Day {
 
     @Override
     public String part1(List<String> input) {
-        return input.isEmpty() ? "" : input.get(0);
+        String text = input.get(0);
+        StringBuilder numbers = new StringBuilder();
+
+        for (String letter : text.split("")) {
+            try{
+                Integer.parseInt(letter);
+                numbers.append(letter);
+            } catch (NumberFormatException e) {
+
+            }
+
+        }
+        return numbers.toString();
     }
 
     @Override
--- src/test/java/aoc/day01/Day01Test.java
+++ src/test/java/aoc/day01/Day01Test.java
@@ -3,6 +3,7 @@ package aoc.day01;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -13,13 +14,30 @@ public class Day01Test {
     @Test
     public void testPart1(){
         // Given
-        List<String> input = Collections.singletonList("test");
+        List<String> input = new  ArrayList<>();
+        input.add("1abc2");
+        input.add("pqr3stu8vwx5");
+        input.add("a1b2c3d4e5f");
+        input.add("treb7uchet");
 
         // When
         String result = new Day01().part1(input);
 
         // Then
-        assertEquals(input.get(0), result);
+        assertEquals("142", result);
+    }
+
+    @Test
+    public void testPart1Line1(){
+        // Given
+        List<String> input = new  ArrayList<>();
+        input.add("1abc2");
+
+        // When
+        String result = new Day01().part1(input);
+
+        // Then
+        assertEquals("12", result);
     }
 
     @Test
