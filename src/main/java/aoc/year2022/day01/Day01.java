772fc443786180267cdfe2b915027e43a1e4f9f6
Syed <72160793+ssa17@users.noreply.github.com>
2024-07-12 11:23:30 +0100

    Complete Day01 2022 part1
    Trying to write code in a functional way.
    
    - [x] character sequence
    - [x] exception flow control
    - [x] list literal
    - [x] multiline
    - [x] Only read first and last digits
    - [x] Refactor for loops
    - [x] Optionals
    - [x] widen
    - [x] move line
    - [x] kill line
    - [x] camelhumps
    - [x] test pyramid
    - [x] immutable list
    - [ ] concurrent modification
    - [x] final parameters
    - [x] change test part two to express how the result is computed
    - [ ] consider test coverage
    - [ ] refactor sumElfCalories to use reduce
    
    Co-authored-by: Mark Crossfield <mark.crossfield@autotrader.co.uk>

--- src/main/java/aoc/year2022/day01/Day01.java
+++ src/main/java/aoc/year2022/day01/Day01.java
@@ -2,16 +2,22 @@ package aoc.year2022.day01;
 
 import aoc.Day;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Day01 extends Day {
-    protected Day01(int dayNumber) {
-        super(dayNumber);
+
+    static {
+        currentDay = new Day01();
+    }
+
+    protected Day01() {
+        super(1);
     }
 
     @Override
     public String part1(List<String> input) {
-        return "";
+        return String.valueOf(sumElfCalories(input).stream().max(Integer::compareTo).orElseThrow());
     }
 
     @Override
@@ -20,6 +26,14 @@ public class Day01 extends Day {
     }
 
     public List<Integer> sumElfCalories(List<String> input) {
-        return input.stream().
+        ArrayList<Integer> calories = new ArrayList<>();
+        calories.add(0);
+        return input.stream().collect(() -> calories, (list, item) -> {
+            if (!item.isEmpty()) {
+                list.set(list.size()-1, list.getLast() + Integer.parseInt(item));
+            } else {
+                list.add(0);
+            };
+        }, List::addAll);
     }
 }
--- src/test/java/aoc/year2022/day01/Day01Test.java
+++ src/test/java/aoc/year2022/day01/Day01Test.java
@@ -13,7 +13,7 @@ public class Day01Test {
 
         List<String> input = List.of("1000", "2000", "3000", "", "4000", "", "5000", "6000", "", "7000", "8000", "9000", "", "10000");
 
-        assertThat(new Day01(1).part1(input)).isEqualTo("24000");
+        assertThat(new Day01().part1(input)).isEqualTo("24000");
     }
 
     @Test
@@ -21,6 +21,6 @@ public class Day01Test {
 
         List<String> input = List.of("1000", "2000", "3000", "", "4000", "", "5000", "6000", "", "7000", "8000", "9000", "", "10000");
 
-        assertThat(new Day01(1).sumElfCalories(input)).containsExactly(6000, 4000, 11000, 24000, 10000);
+        assertThat(new Day01().sumElfCalories(input)).containsExactly(6000, 4000, 11000, 24000, 10000);
     }
 }
